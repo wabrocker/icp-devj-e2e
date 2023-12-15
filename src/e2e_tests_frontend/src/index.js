7bd8399cf67231b615ed76c0dd7df63550b7666e
wabrocker <billbrocker@gmail.com>
2023-12-15 18:20:45 -0500

    Initial commit.

---
+++ src/e2e_tests_frontend/src/index.js
@@ -0,0 +1,19 @@
+import { e2e_tests_backend } from "../../declarations/e2e_tests_backend";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await e2e_tests_backend.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
